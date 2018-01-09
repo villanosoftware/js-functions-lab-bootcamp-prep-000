211cbf43730424b1030f1369a1062e841bdb4d66
Alex Villano <alex.villano@yahoo.com>
2018-01-09 17:43:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}`
 }
 
-function happyHolidayTo(name, day) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${day}, ${name}`
 }
 
